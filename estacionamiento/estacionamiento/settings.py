4f8cd299b30ce21625169b47f9a3f3cc12e479e5
oscar-L97 <oscarlopezg97@gmail.com>
2018-12-05 16:23:22 -0800

    arreglos botones submit

--- estacionamiento/estacionamiento/settings.py
+++ estacionamiento/estacionamiento/settings.py
@@ -77,8 +77,14 @@ WSGI_APPLICATION = 'estacionamiento.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'estacionamiento',
+        'USER': 'twitterclone_user2',
+        'PASSWORD': 'ContraTwitter123',
+        'HOST': 'localhost',
+        'PORT': '3306'
     }
 }
 
